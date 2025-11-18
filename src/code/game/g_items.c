a2b3ad8844e36b424b1c117b7ddfcb4c60804222
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-18 14:43:03 +0000

    Rework item pickup gating

--- src/code/game/g_items.c
+++ src/code/game/g_items.c
@@ -657,7 +657,7 @@ void Touch_Item (gentity_t *ent, gentity_t *other, trace_t *trace) {
 		return;		// dead people can't pickup
 
 	// the same pickup rules are used for client side and server side
-	if ( !BG_CanItemBeGrabbed( g_gametype.integer, &ent->s, &other->client->ps ) ) {
+	if ( !BG_CanItemBeGrabbed( g_gametype.integer, level.time, &ent->s, &other->client->ps ) ) {
 		return;
 	}
 
