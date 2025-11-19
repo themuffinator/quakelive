2c9b75d4329258caf2912cdc39b5ba384b651670
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-19 17:25:26 +0000

    Update Quake Live game version strings

--- src/code/game/g_local.h
+++ src/code/game/g_local.h
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"baseq3"
+#define	GAMEVERSION	"baseqz"
 
 #define BODY_QUEUE_SIZE		8
 
