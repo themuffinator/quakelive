529f2dabc61db951d5676dfb6f544a608d128e3f
themuffinator <djdacosta86@yahoo.co.uk>
2025-12-12 19:43:46 +0000

    Map level_locals_t offsets from HLIL references

--- src/game/ql_game_types.h
+++ src/game/ql_game_types.h
@@ -71,19 +71,217 @@ typedef struct ql_gentity_s {
 } ql_gentity_t;
 
 typedef struct ql_level_locals_s {
-    ql_gclient_t *clients;             // 0x0000
-    ql_gentity_t *gentities;           // 0x0004
-    int32_t gentity_size;              // 0x0008
-    int32_t gclient_size;              // 0x000C
-    int32_t maxclients;                // 0x0010
-    int32_t framenum;                  // 0x0014
-    int32_t time;                      // 0x0018
-    int32_t previousTime;              // 0x001C
-    uint8_t __pad0[0x134 - 0x020];
-    int32_t logFile;                   // 0x0134
-    uint8_t __pad1[0x1C0 - 0x138];
-    int32_t warmup_restart_latch;      // 0x01C0
-    uint8_t __pad2[0x4B9C - 0x1C4];
+	ql_gclient_t *	clients;		// 0x0000
+	ql_gentity_t *	gentities;		// 0x0004
+	int32_t gentity_size;			// 0x0008
+	int32_t	gclient_size;		// 0x000C
+	int32_t	maxclients;		// 0x0010
+	int32_t	framenum;		// 0x0014
+	int32_t	time;		// 0x0018
+	int32_t	previousTime;		// 0x001C
+	int32_t	field_0020;		// 0x0020
+	int32_t	field_0024;		// 0x0024
+	int32_t	field_0028;		// 0x0028
+	int32_t	field_002c;		// 0x002C
+	int32_t	field_0030;		// 0x0030
+	uint8_t __pad0034[0x0004];
+	int32_t	field_0038;		// 0x0038
+	int32_t	field_003c;		// 0x003C
+	int32_t	field_0040;		// 0x0040
+	int32_t	field_0044;		// 0x0044
+	int32_t	field_0048;		// 0x0048
+	int32_t	field_004c;		// 0x004C
+	int32_t	field_0050;		// 0x0050
+	int32_t	field_0054;		// 0x0054
+	int32_t	field_0058;		// 0x0058
+	int32_t	field_005c;		// 0x005C
+	int32_t	field_0060;		// 0x0060
+	int32_t	field_0064;		// 0x0064
+	int32_t	field_0068;		// 0x0068
+	uint8_t __pad006c[0x00F8];
+	int32_t	follow1;		// 0x0164
+	int32_t	follow2;		// 0x0168
+	int32_t	snd_fry;		// 0x016C
+	int32_t	field_0170;		// 0x0170
+	int32_t	field_0174;		// 0x0174
+	uint8_t __pad0178[0x07FC];
+	int32_t	field_0974;		// 0x0974
+	int32_t	field_0978;		// 0x0978
+	int32_t	field_097c;		// 0x097C
+	int32_t	field_0980;		// 0x0980
+	int32_t	field_0984;		// 0x0984
+	int32_t	field_0988;		// 0x0988
+	int32_t	field_098c;		// 0x098C
+	int32_t	field_0990;		// 0x0990
+	int32_t	field_0994;		// 0x0994
+	uint8_t __pad0998[0x01F8];
+	int32_t	field_0b90;		// 0x0B90
+	uint8_t __pad0b94[0x1000];
+	int32_t	field_1b94;		// 0x1B94
+	int32_t	field_1b98;		// 0x1B98
+	int32_t	field_1b9c;		// 0x1B9C
+	int32_t	field_1ba0;		// 0x1BA0
+	int32_t	field_1ba4;		// 0x1BA4
+	int32_t	field_1ba8;		// 0x1BA8
+	int32_t	field_1bac;		// 0x1BAC
+	int32_t	field_1bb0;		// 0x1BB0
+	int32_t	field_1bb4;		// 0x1BB4
+	int32_t	field_1bb8;		// 0x1BB8
+	int32_t	field_1bbc;		// 0x1BBC
+	int32_t	field_1bc0;		// 0x1BC0
+	int32_t	field_1bc4;		// 0x1BC4
+	int32_t	field_1bc8;		// 0x1BC8
+	int32_t	field_1bcc;		// 0x1BCC
+	int32_t	field_1bd0;		// 0x1BD0
+	int32_t	field_1bd4;		// 0x1BD4
+	int32_t	field_1bd8;		// 0x1BD8
+	int32_t	field_1bdc;		// 0x1BDC
+	uint8_t __pad1be0[0x001C];
+	int32_t	field_1bfc;		// 0x1BFC
+	int32_t	field_1c00;		// 0x1C00
+	int32_t	field_1c04;		// 0x1C04
+	int32_t	field_1c08;		// 0x1C08
+	int32_t	field_1c0c;		// 0x1C0C
+	uint8_t __pad1c10[0x03FF];
+	uint8_t	field_200f;		// 0x200F
+	uint8_t __pad2010[0x03FF];
+	uint8_t	field_240f;		// 0x240F
+	int32_t	field_2410;		// 0x2410
+	uint8_t __pad2414[0x0024];
+	int32_t	field_2438;		// 0x2438
+	uint8_t __pad243c[0x0024];
+	int32_t	field_2460;		// 0x2460
+	uint8_t __pad2464[0x0024];
+	int32_t	field_2488;		// 0x2488
+	uint8_t __pad248c[0x0024];
+	int32_t	field_24b0;		// 0x24B0
+	int32_t	field_24b4;		// 0x24B4
+	int32_t	field_24b8;		// 0x24B8
+	int32_t	field_24bc;		// 0x24BC
+	int32_t	field_24c0;		// 0x24C0
+	int32_t	field_24c4;		// 0x24C4
+	int32_t	field_24c8;		// 0x24C8
+	int32_t	field_24cc;		// 0x24CC
+	int32_t	field_24d0;		// 0x24D0
+	int32_t	field_24d4;		// 0x24D4
+	int32_t	field_24d8;		// 0x24D8
+	int32_t	field_24dc;		// 0x24DC
+	uint8_t __pad24e0[0x000C];
+	int32_t	field_24ec;		// 0x24EC
+	uint8_t __pad24f0[0x00EC];
+	int32_t	field_25dc;		// 0x25DC
+	int32_t	field_25e0;		// 0x25E0
+	int32_t	field_25e4;		// 0x25E4
+	int32_t	field_25e8;		// 0x25E8
+	int32_t	field_25ec;		// 0x25EC
+	uint8_t __pad25f0[0x0004];
+	int32_t	field_25f4;		// 0x25F4
+	int32_t	field_25f8;		// 0x25F8
+	int32_t	field_25fc;		// 0x25FC
+	uint8_t __pad2600[0x0004];
+	int32_t	field_2604;		// 0x2604
+	int32_t	field_2608;		// 0x2608
+	int32_t	field_260c;		// 0x260C
+	uint8_t __pad2610[0x0004];
+	int32_t	field_2614;		// 0x2614
+	int32_t	field_2618;		// 0x2618
+	int32_t	field_261c;		// 0x261C
+	uint8_t __pad2620[0x0004];
+	int32_t	field_2624;		// 0x2624
+	int32_t	field_2628;		// 0x2628
+	int32_t	field_262c;		// 0x262C
+	uint8_t __pad2630[0x0004];
+	int32_t	field_2634;		// 0x2634
+	int32_t	field_2638;		// 0x2638
+	int32_t	field_263c;		// 0x263C
+	uint8_t __pad2640[0x0004];
+	int32_t	field_2644;		// 0x2644
+	int32_t	field_2648;		// 0x2648
+	int32_t	field_264c;		// 0x264C
+	uint8_t __pad2650[0x0004];
+	int32_t	field_2654;		// 0x2654
+	int32_t	field_2658;		// 0x2658
+	int32_t	field_265c;		// 0x265C
+	uint8_t __pad2660[0x0004];
+	int32_t	field_2664;		// 0x2664
+	int32_t	field_2668;		// 0x2668
+	int32_t	field_266c;		// 0x266C
+	uint8_t __pad2670[0x0004];
+	int32_t	field_2674;		// 0x2674
+	int32_t	field_2678;		// 0x2678
+	int32_t	field_267c;		// 0x267C
+	uint8_t __pad2680[0x0004];
+	int32_t	field_2684;		// 0x2684
+	int32_t	field_2688;		// 0x2688
+	int32_t	field_268c;		// 0x268C
+	uint8_t __pad2690[0x0004];
+	int32_t	field_2694;		// 0x2694
+	int32_t	field_2698;		// 0x2698
+	int32_t	field_269c;		// 0x269C
+	uint8_t __pad26a0[0x0004];
+	int32_t	field_26a4;		// 0x26A4
+	int32_t	field_26a8;		// 0x26A8
+	int32_t	field_26ac;		// 0x26AC
+	uint8_t __pad26b0[0x0004];
+	int32_t	field_26b4;		// 0x26B4
+	int32_t	field_26b8;		// 0x26B8
+	int32_t	field_26bc;		// 0x26BC
+	uint8_t __pad26c0[0x0004];
+	int32_t	field_26c4;		// 0x26C4
+	int32_t	field_26c8;		// 0x26C8
+	int32_t	field_26cc;		// 0x26CC
+	uint8_t __pad26d0[0x0004];
+	int32_t	field_26d4;		// 0x26D4
+	int32_t	field_26d8;		// 0x26D8
+	int32_t	field_26dc;		// 0x26DC
+	uint8_t __pad26e0[0x0004];
+	int32_t	field_26e4;		// 0x26E4
+	int32_t	field_26e8;		// 0x26E8
+	int32_t	field_26ec;		// 0x26EC
+	uint8_t __pad26f0[0x0004];
+	int32_t	field_26f4;		// 0x26F4
+	uint8_t __pad26f8[0x0010];
+	int32_t	field_2708;		// 0x2708
+	int32_t	field_270c;		// 0x270C
+	int32_t	field_2710;		// 0x2710
+	int32_t	field_2714;		// 0x2714
+	int32_t	field_2718;		// 0x2718
+	uint8_t __pad271c[0x0004];
+	int32_t	field_2720;		// 0x2720
+	uint8_t __pad2724[0x0008];
+	int32_t	field_272c;		// 0x272C
+	int32_t	field_2730;		// 0x2730
+	int32_t	field_2734;		// 0x2734
+	int32_t	field_2738;		// 0x2738
+	int32_t	field_273c;		// 0x273C
+	int32_t	field_2740;		// 0x2740
+	int32_t	field_2744;		// 0x2744
+	int32_t	field_2748;		// 0x2748
+	int32_t	field_274c;		// 0x274C
+	int32_t	field_2750;		// 0x2750
+	int32_t	field_2754;		// 0x2754
+	int32_t	field_2758;		// 0x2758
+	int32_t	field_275c;		// 0x275C
+	int32_t	field_2760;		// 0x2760
+	int32_t	field_2764;		// 0x2764
+	int32_t	field_2768;		// 0x2768
+	int32_t	field_276c;		// 0x276C
+	int32_t	field_2770;		// 0x2770
+	int32_t	field_2774;		// 0x2774
+	int32_t	field_2778;		// 0x2778
+	uint8_t __pad277c[0x03FC];
+	int32_t	field_2b78;		// 0x2B78
+	uint8_t __pad2b7c[0x1FF8];
+	int32_t	field_4b74;		// 0x4B74
+	int32_t	field_4b78;		// 0x4B78
+	int32_t	field_4b7c;		// 0x4B7C
+	int32_t	field_4b80;		// 0x4B80
+	int32_t	field_4b84;		// 0x4B84
+	int32_t	field_4b88;		// 0x4B88
+	int32_t	field_4b8c;		// 0x4B8C
+	int32_t	field_4b90;		// 0x4B90
+	int32_t	field_4b94;		// 0x4B94
+	int32_t	field_4b98;		// 0x4B98
 } ql_level_locals_t;
 
 typedef ql_gclient_t gclient_t;
@@ -126,8 +324,6 @@ QL_STATIC_ASSERT(offsetof(ql_level_locals_t, maxclients) == 0x0010, "level_local
 QL_STATIC_ASSERT(offsetof(ql_level_locals_t, framenum) == 0x0014, "level_locals_t.framenum offset");
 QL_STATIC_ASSERT(offsetof(ql_level_locals_t, time) == 0x0018, "level_locals_t.time offset");
 QL_STATIC_ASSERT(offsetof(ql_level_locals_t, previousTime) == 0x001C, "level_locals_t.previousTime offset");
-QL_STATIC_ASSERT(offsetof(ql_level_locals_t, logFile) == 0x0134, "level_locals_t.logFile offset");
-QL_STATIC_ASSERT(offsetof(ql_level_locals_t, warmup_restart_latch) == 0x01C0, "level_locals_t.warmup_restart_latch offset");
 QL_STATIC_ASSERT(sizeof(ql_level_locals_t) == 0x4B9C, "level_locals_t size");
 
 #undef QL_STATIC_ASSERT
