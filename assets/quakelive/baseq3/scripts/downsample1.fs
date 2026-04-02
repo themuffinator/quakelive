8dd9a084e2a42d7ba4b2a36b16c1fa1e0fa32a6a
themuffinator <djdacosta86@yahoo.co.uk>
2026-04-02 13:17:04 +0100

    Delete assets directory

--- assets/quakelive/baseq3/scripts/downsample1.fs
+++
@@ -1,8 +0,0 @@
-uniform sampler2DRect backBufferTex;
-
-void main()
-{
-	//vec4 color = texture2DRect(backBufferTex, gl_TexCoord[0].xy * 2.0 );
-	vec4 color = texture2DRect(backBufferTex, gl_TexCoord[1].xy );
-	gl_FragColor = color;
-}
